2758e41649fc509f5da24067ab2b76e7c5c45047
June Roh <june.roh@streetsmarket.com>
2022-11-21 15:54:13 -0500

    add detail

--- api.ts
+++ api.ts
@@ -4,86 +4,98 @@ const API_KEY = "c183a5884681c02c32b9c0dad01728ba";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export interface IMovie {
-  adult: boolean;
-  backdrop_path?: string;
-  genre_ids: number[];
-  id: number;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
-  poster_path?: string;
-  release_date: string;
-  title: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
+    adult: boolean;
+    backdrop_path?: string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path?: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
 }
 
 export interface ITv {
-  poster_path?: string;
-  popularity: number;
-  id: number;
-  backdrop_path?: string;
-  vote_average: number;
-  overview: string;
-  first_air_date: string;
-  origin_country: string[];
-  genre_ids: number[];
-  original_language: string;
-  vote_count: number;
-  name: string;
-  original_name: string;
+    poster_path?: string;
+    popularity: number;
+    id: number;
+    backdrop_path?: string;
+    vote_average: number;
+    overview: string;
+    first_air_date: string;
+    origin_country: string[];
+    genre_ids: number[];
+    original_language: string;
+    vote_count: number;
+    name: string;
+    original_name: string;
 }
 
 interface IBaseResponse {
-  page: number;
-  total_results: number;
-  total_pages: number;
+    page: number;
+    total_results: number;
+    total_pages: number;
 }
 
 export interface IMovieResponse extends IBaseResponse {
-  results: IMovie[];
+    results: IMovie[];
 }
 
 export const moviesAPI = {
-  trending: () =>
-    fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`).then(
-      (response) => response.json()
-    ),
-  upcoming: () =>
-    fetch(
-      `${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
-    ).then((response) => response.json()),
-  nowPlaying: () =>
-    fetch(
-      `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
-    ).then((response) => response.json()),
-  search: ({ queryKey }: QueryFunctionContext) => {
-    const [_, query] = queryKey;
-    return fetch(
-      `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=1&query=${query}`
-    ).then((response) => response.json());
-  },
+    trending: () =>
+        fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`).then(
+            (response) => response.json()
+        ),
+    upcoming: () =>
+        fetch(
+            `${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
+        ).then((response) => response.json()),
+    nowPlaying: () =>
+        fetch(
+            `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
+        ).then((response) => response.json()),
+    search: ({ queryKey }: QueryFunctionContext) => {
+        const [_, query] = queryKey;
+        return fetch(
+            `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=1&query=${query}`
+        ).then((response) => response.json());
+    },
+    detail: ({ queryKey }: QueryFunctionContext) => {
+        const [_, id] = queryKey;
+        return fetch(
+            `${BASE_URL}/movie/${id}?api_key=${API_KEY}&append_to_response=videos,images`
+        ).then((response) => response.json());
+    },
 };
 
 export const tvAPI = {
-  trending: () =>
-    fetch(`${BASE_URL}/trending/tv/week?api_key=${API_KEY}`).then((response) =>
-      response.json()
-    ),
-  airingToday: () =>
-    fetch(`${BASE_URL}/tv/airing_today?api_key=${API_KEY}`).then((response) =>
-      response.json()
-    ),
-  topRated: () =>
-    fetch(`${BASE_URL}/tv/top_rated?api_key=${API_KEY}`).then((response) =>
-      response.json()
-    ),
-  search: ({ queryKey }: QueryFunctionContext) => {
-    const [_, query] = queryKey;
-    return fetch(
-      `${BASE_URL}/search/tv?api_key=${API_KEY}&language=en-US&page=1&query=${query}`
-    ).then((response) => response.json());
-  },
+    trending: () =>
+        fetch(`${BASE_URL}/trending/tv/week?api_key=${API_KEY}`).then(
+            (response) => response.json()
+        ),
+    airingToday: () =>
+        fetch(`${BASE_URL}/tv/airing_today?api_key=${API_KEY}`).then(
+            (response) => response.json()
+        ),
+    topRated: () =>
+        fetch(`${BASE_URL}/tv/top_rated?api_key=${API_KEY}`).then((response) =>
+            response.json()
+        ),
+    search: ({ queryKey }: QueryFunctionContext) => {
+        const [_, query] = queryKey;
+        return fetch(
+            `${BASE_URL}/search/tv?api_key=${API_KEY}&language=en-US&page=1&query=${query}`
+        ).then((response) => response.json());
+    },
+    detail: ({ queryKey }: QueryFunctionContext) => {
+        const [_, id] = queryKey;
+        return fetch(
+            `${BASE_URL}/tv/${id}?api_key=${API_KEY}&append_to_response=videos,images`
+        ).then((response) => response.json());
+    },
 };
