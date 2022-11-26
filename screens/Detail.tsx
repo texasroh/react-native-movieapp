d0ca662241f130b38fcd9a76cac773b654c8306e
June Roh <texasroh@gmail.com>
2022-11-26 00:45:41 -0500

    add share button

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,6 +1,12 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect } from "react";
-import { Dimensions, Linking, StyleSheet } from "react-native";
+import {
+  Dimensions,
+  Linking,
+  Share,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import styled from "styled-components/native";
 import { IMovie, ITv, moviesAPI, tvAPI } from "../api";
 import Poster from "../components/Poster";
@@ -14,109 +20,130 @@ import { Ionicons } from "@expo/vector-icons";
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Container = styled.ScrollView`
-    background-color: ${(props) => props.theme.mainBgColor};
+  background-color: ${(props) => props.theme.mainBgColor};
 `;
 
 const Header = styled.View`
-    height: ${SCREEN_HEIGHT / 4}px;
-    justify-content: flex-end;
-    padding: 0px 20px;
+  height: ${SCREEN_HEIGHT / 4}px;
+  justify-content: flex-end;
+  padding: 0px 20px;
 `;
 
 const Background = styled.Image``;
 
 const Column = styled.View`
-    flex-direction: row;
+  flex-direction: row;
 `;
 
 const Title = styled.Text`
-    color: white;
-    font-size: 24px;
-    align-self: flex-end;
-    flex-shrink: 1;
-    margin-left: 15px;
-    font-weight: 500;
+  color: white;
+  font-size: 24px;
+  align-self: flex-end;
+  flex-shrink: 1;
+  margin-left: 15px;
+  font-weight: 500;
 `;
 
 const Data = styled.View`
-    padding: 0 20px;
+  padding: 0 20px;
 `;
 
 const Overview = styled.Text`
-    color: ${(props) => props.theme.textColor};
-    margin: 20px 0;
+  color: ${(props) => props.theme.textColor};
+  margin: 20px 0;
 `;
 
 const VideoBtn = styled.TouchableOpacity`
-    flex-direction: row;
+  flex-direction: row;
 `;
 const BtnText = styled.Text`
-    color: white;
-    font-weight: 600;
-    margin-bottom: 10px;
-    line-height: 24px;
-    margin-left: 10px;
+  color: white;
+  font-weight: 600;
+  margin-bottom: 10px;
+  line-height: 24px;
+  margin-left: 10px;
 `;
 
 type RootStackParamList = {
-    Detail: IMovie | ITv;
+  Detail: IMovie | ITv;
 };
 
 type DetailScreenProps = NativeStackScreenProps<RootStackParamList, "Detail">;
 
 const Detail: React.FC<DetailScreenProps> = ({
-    navigation: { setOptions },
-    route: { params },
+  navigation: { setOptions },
+  route: { params },
 }) => {
-    const isMovie = "original_title" in params;
-    const { isLoading, data } = useQuery(
-        [isMovie ? "movies" : "tvs", params.id],
-        isMovie ? moviesAPI.detail : tvAPI.detail
-    );
-    const originalTitle = params.original_title ?? params.original_name;
+  const isMovie = "original_title" in params;
+  const { isLoading, data } = useQuery(
+    [isMovie ? "movies" : "tvs", params.id],
+    isMovie ? moviesAPI.detail : tvAPI.detail
+  );
+  const originalTitle = params.original_title ?? params.original_name;
+  const shareMedia = async () => {
+    await Share.share({
+      url: isMovie
+        ? `https://www.imdb.com/title/${data.imdb_id}`
+        : data.homepage,
+      message: `${params.overview}\nCheck it out: ${
+        isMovie ? `https://www.imdb.com/title/${data.imdb_id}` : data.homepage
+      }`,
+      title: originalTitle,
+    });
+  };
+  const ShareButton = () => (
+    <TouchableOpacity onPress={shareMedia}>
+      <Ionicons name="share-outline" color="white" size={24} />
+    </TouchableOpacity>
+  );
 
-    useEffect(() => {
-        setOptions({
-            title: "original_title" in params ? "Movie" : "TV Show",
-        });
-    }, []);
+  useEffect(() => {
+    setOptions({
+      title: "original_title" in params ? "Movie" : "TV Show",
+    });
+  }, []);
 
-    const openYTLink = async (videoID: string) => {
-        const baseUrl = `https://m.youtube.com/watch?v=${videoID}`;
-        await Linking.openURL(baseUrl);
-    };
-    return (
-        <Container>
-            <Header>
-                <Background
-                    style={StyleSheet.absoluteFill}
-                    source={{ uri: makeImgPath(params.backdrop_path || "") }}
-                />
-                <LinearGradient
-                    // Background Linear Gradient
-                    colors={["transparent", BLACK_COLOR]}
-                    style={StyleSheet.absoluteFill}
-                />
-                <Column>
-                    <Poster path={params.poster_path || ""} />
-                    <Title>{originalTitle}</Title>
-                </Column>
-            </Header>
-            <Data>
-                <Overview>{params.overview}</Overview>
-                {isLoading ? <Loader /> : null}
-                {data?.videos?.results?.map((video) => (
-                    <VideoBtn
-                        key={video.key}
-                        onPress={() => openYTLink(video.key)}
-                    >
-                        <Ionicons name="logo-youtube" color="white" size={24} />
-                        <BtnText>{video.name}</BtnText>
-                    </VideoBtn>
-                ))}
-            </Data>
-        </Container>
-    );
+  useEffect(() => {
+    if (data) {
+      setOptions({
+        headerRight: () => <ShareButton />,
+      });
+    }
+  }, [data]);
+
+  const openYTLink = async (videoID: string) => {
+    const baseUrl = `https://m.youtube.com/watch?v=${videoID}`;
+    await Linking.openURL(baseUrl);
+  };
+  return (
+    <Container>
+      <Header>
+        <Background
+          style={StyleSheet.absoluteFill}
+          source={{ uri: makeImgPath(params.backdrop_path || "") }}
+        />
+        <LinearGradient
+          // Background Linear Gradient
+          colors={["transparent", BLACK_COLOR]}
+          style={StyleSheet.absoluteFill}
+        />
+        <Column>
+          <Poster path={params.poster_path || ""} />
+          <Title>{originalTitle}</Title>
+        </Column>
+      </Header>
+      <Data>
+        <Overview>{params.overview}</Overview>
+        {isLoading ? <Loader /> : null}
+        {data?.videos?.results?.map((video) => (
+          <VideoBtn key={video.key} onPress={() => openYTLink(video.key)}>
+            <Ionicons name="logo-youtube" color="white" size={24} />
+            <BtnText>{video.name}</BtnText>
+          </VideoBtn>
+        ))}
+      </Data>
+    </Container>
+  );
 };
 
 export default Detail;
